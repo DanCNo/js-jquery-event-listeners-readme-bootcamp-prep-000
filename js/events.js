4d9e251c51d6a1eb4d90405a73900779db9d60e2
Dan N <dan.noh@gmail.com>
2018-08-24 19:40:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,6 +1,22 @@
 //define functions here
 
+function getIt() {
+  $('p').on("click", function() {
+    alert('Hey!');
+  })
+}
 
+function frameIt() {
+  $('img').on('load', function() {
+    
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which == )
+  })
+}
 
 $(document).ready(function(){
 
